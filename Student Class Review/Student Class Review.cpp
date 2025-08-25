6672d53d2aa427ff1181f0e285690d0c86626f3a
JAMitOut <Jonas007motto@gmail.com>
2025-08-25 17:16:58 -0500

    Working Version of the Student Class Review.

--- Student Class Review/Student Class Review.cpp
+++ Student Class Review/Student Class Review.cpp
@@ -1,46 +1,77 @@
 #include <iostream>
 #include <cassert>
 #include <ctime>
+#include <string>
+#include <cstdlib>
 
 using namespace std;
 
 class Student {
+private:
+	string first_name;
+	string last_name;
+	int student_id, number_of_credits;
+
+	int generate_id() {
+		return rand() % 900000 + 100000;
+	}
+
 public:
-	string student_name_first;
-	string student_name_last;
-
-	void MyStudent(string first_name, string last_name) {
-		student_name_first = first_name;
-		student_name_last = last_name;
-	}
-	
-
-	void generate_Id() {
-		srand(static_cast<unsigned int>(time(nullptr)))
-
-		int random_Id = (rand() % 9) + 1;
+	//Constructor
+	Student(const string& first, const string& last)
+		: first_name(first), last_name(last), number_of_credits(0) {
+		student_id = generate_id();
 	}
 
-}
+	//Getters
+	string get_first_name() const { return first_name; }
+	string get_last_name() const { return last_name; }
+	int get_id() const { return student_id; }
+	int get_number_of_credits() const { return number_of_credits; }
+
+	//Setters
+	void set_first_name(const string& first) { first_name = first; }
+	void set_last_name(const string& last) { last_name = last; }
+
+	//Methods
+	void add_credits(int number) {
+		if (number > 0) {
+			number_of_credits += number;
+		}
+	}
+
+	void reset_credits() {
+		number_of_credits = 0;
+	}
+};
 
 int main() {
-	assert(student.first_name == "Bob");
-	assert(student.last_name == "Bobberson");
-	assert(student.number_of_credits == 0);
+	srand(static_cast<unsigned>(time(0)));
+
+	Student student("Bob", "Bobberson");
+
+	assert(student.get_first_name() == "Bob");
+	assert(student.get_last_name() == "Bobberson");
+
+	assert(student.get_number_of_credits() == 0);
 
 	student.add_credits(6);
 	student.add_credits(2);
-
-	assert(student.number_of_credits == 8);
+	assert(student.get_number_of_credits() == 8);
 
 	student.reset_credits();
+	assert(student.get_number_of_credits() == 0);
 
-	assert(student.number_of_credits == 0);
+	student.set_first_name("Bab");
+	student.set_last_name("Babberson");
+	assert(student.get_first_name() == "Bab");
+	assert(student.get_last_name() == "Babberson");
 
+	cout << "Student: " << student.get_first_name() << " "
+		<< student.get_last_name() << "\nID: "
+		<< student.get_id() << "\nCredits: "
+		<< student.get_number_of_credits() << endl;
 
-	MyStudent obj("Bob", "Bobberson");
-
-	cout << "Your ID: " << student.generate_Id;
-
+	cout << "All Tests Passed Successfully!" << endl;
 	return 0;
 }
